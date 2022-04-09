0e180377d99deed3e94416f5279182defb0a9b9e
Minh Huynh <32327317+minhhuynh1812@users.noreply.github.com>
2022-04-09 10:57:59 +0700

    Hủy câu các ký tự đặc biệt

--- js/ConvertVie.js
+++ js/ConvertVie.js
@@ -24,6 +24,6 @@ function removeVietnameseTones(str) {
     str = str.trim();
     // Remove punctuations
     // Bỏ dấu câu, kí tự đặc biệt
-    str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g," ");
+    //str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g," ");
     return str;
 }
